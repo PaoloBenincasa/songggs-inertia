341a0eb81759316501768aaf1bbb471efae828e6
Paolo Benincasa <paolobenincasa1984@gmail.com>
2025-02-19 09:48:17 +0100

    start

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Avvia il server Laravel
+php artisan serve --host=0.0.0.0 --port=3000 &
+
+# Costruisce il frontend (React) per la produzione
+npm run build
+
+# Servi i file statici prodotti dal frontend React (se usi Laravel per servire il frontend)
+php -S 0.0.0.0:3001 -t public
